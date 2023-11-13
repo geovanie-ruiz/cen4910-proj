6d7f07831896fba8f92e87528ea63582fa016f87
KennethTillit <Kennytillit226@gmail.com>
2023-11-12 20:58:59 -0500

    Update auth.module.ts

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -4,12 +4,29 @@ import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { jwtConstants } from './constants/constants';
 import { JwtModule } from '@nestjs/jwt';
+import * as Joi from 'joi';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(4000),
+        NODE_ENV: Joi.string().default('development'),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+        DB_SSL: Joi.boolean().required(),
+      }),
+    }),
+    DatabaseModule,
     UsersModule,
+    AuthModule,
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
